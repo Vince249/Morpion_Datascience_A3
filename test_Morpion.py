6b74402a174a5b61877f267d78edb2a9f49d0f46
RemiEC <guillonbonyremi@gmail.com>
2020-03-26 14:39:48 +0100

    Setup Virtual Environment + Split unique File into different ones according to their purpose

--- test_Morpion.py
+++ test_Morpion.py
@@ -1,59 +1,60 @@
 import unittest
 import Morpion
+import Fonctions_de_base
 
-class TestMorpionAction(unittest.TestCase):
+class Test_Fonctions_de_base_Action(unittest.TestCase):
 
     def testAction(self):
-        liste = Morpion.Action([['X','',''],['O','X',''],['','O','O']], 'X')
+        liste = Fonctions_de_base.Action([['X','',''],['O','X',''],['','O','O']], 'X')
         self.assertEqual(liste,[['X', 0, 1], ['X', 0, 2], ['X', 1, 2], ['X', 2, 0]])
 
 
-class TestMorpionResult(unittest.TestCase):
+class Test_Fonctions_de_base_Result(unittest.TestCase):
 
     def testResult(self):
         liste = [['X','',''],['O','X',''],['','O','O']]
-        liste = Morpion.Result(liste,['X', 0, 1])
+        liste = Fonctions_de_base.Result(liste,['X', 0, 1])
         self.assertEqual(liste, [['X','X',''],['O','X',''],['','O','O']])
 
 
-class TestMorpionTerminal(unittest.TestCase):
+class Test_Fonctions_de_base_Terminal(unittest.TestCase):
 
     def testTerminalPasFini(self):
         state = [['X','O','X'],['','',''],['','O','O']]
-        self.assertFalse(Morpion.Terminal_Test(state,3))
+        self.assertFalse(Fonctions_de_base.Terminal_Test(state,3))
 
     def testTerminalPlein(self):
         state = [['X','O','X'],['O','X','X'],['X','O','O']]
-        self.assertTrue(Morpion.Terminal_Test(state,3))
+        self.assertTrue(Fonctions_de_base.Terminal_Test(state,3))
 
     def testTerminalLigne(self):
         state = [['X','X','X'],['O','X',''],['','O','O']]
-        self.assertTrue(Morpion.Terminal_Test(state,3))
+        self.assertTrue(Fonctions_de_base.Terminal_Test(state,3))
     
     def testTerminalColonnes(self):
         state = [['X','',''],['X','X',''],['X','O','O']]
-        self.assertTrue(Morpion.Terminal_Test(state,3))
+        self.assertTrue(Fonctions_de_base.Terminal_Test(state,3))
     
     def testTerminalDiagonal(self):
         state = [['X','X','O'],['O','X',''],['','O','X']]
-        self.assertTrue(Morpion.Terminal_Test(state,3))
+        self.assertTrue(Fonctions_de_base.Terminal_Test(state,3))
 
 
-class TestMorpionUtility(unittest.TestCase):
+class Test_Fonctions_de_base_Utility(unittest.TestCase):
 
     def testUtility1(self):
         liste = [['X','X','X'],['O','X','O'],['X','O','O']]
-        result = Morpion.Utility(liste,'X')
+        result = Fonctions_de_base.Utility(liste,'X')
         self.assertEqual(result, 1)
 
     def testUtilitymoins1(self):
         liste = [['X','X','O'],['X','O','O'],['O','X','']]
-        result = Morpion.Utility(liste,'X')
+        result = Fonctions_de_base.Utility(liste,'X')
         self.assertEqual(result, -1)
 
     def testUtility0(self):
         liste = [['O','X','O'],['O','X','O'],['X','O','X']]
-        result = Morpion.Utility(liste,'X')
+        result = Fonctions_de_base.Utility(liste,'X')
         self.assertEqual(result, 0)
 
 if __name__ == '__main__':
